2cc8377c079f469c7d4a9cd981d9099ae12334d8
volodymyrhudyma <volodymyrhudyma@gmail.com>
2021-05-06 21:28:09 +0200

    Change[2]

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p></p>
         <a
           className='App-link'
           href='https://reactjs.org'
